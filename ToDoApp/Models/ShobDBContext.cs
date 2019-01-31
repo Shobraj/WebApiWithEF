ee057defc8dfd5521d277ef9a37e5b8e1f30953a
Shobharaj <36963737+Shobraj@users.noreply.github.com>
2019-02-01 01:38:07 +0530

    Removed connection string

--- ToDoApp/Models/ShobDBContext.cs
+++ ToDoApp/Models/ShobDBContext.cs
@@ -21,7 +21,7 @@ namespace ToDoApp.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ShobDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("**Connection string**");
             }
         }
 
